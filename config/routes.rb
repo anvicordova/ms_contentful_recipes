691e2d07d83b1468b4cd6ffee6452dae57df747a
Angela Cordova <angela@fishbrain.com>
2022-02-14 21:39:17 +0100

    Add custom error views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes, only: %i[index show]
+
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server'
+  get '/422', to: 'errors#unprocessable'
 end
